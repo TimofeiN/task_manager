f5da1d591c15328438a217f30dea85e07fa71565
Timofei <timofejnazarov@gmail.com>
2023-04-26 13:39:29 +0400

    remove duplicate fields from user model

--- main/models/user.py
+++ main/models/user.py
@@ -8,9 +8,6 @@ class User(AbstractUser):
         MANAGER = "manager"
         ADMIN = "admin"
 
-    name = models.CharField(max_length=50, blank=True)
-    surname = models.CharField(max_length=50, blank=True)
-    email = models.EmailField(blank=True)
     role = models.CharField(
         max_length=50, default=Roles.DEVELOPER, choices=Roles.choices
     )
