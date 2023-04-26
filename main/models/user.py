0ed1af289d9516bbba83d8618f410afe1be5761a
Timofei <timofejnazarov@gmail.com>
2023-04-26 12:43:22 +0400

    Models task complete & files reformatted with black

--- main/models/user.py
+++ main/models/user.py
@@ -1,5 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    class Roles(models.TextChoices):
+        DEVELOPER = "developer"
+        MANAGER = "manager"
+        ADMIN = "admin"
+
+    name = models.CharField(max_length=50, blank=True)
+    surname = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(blank=True)
+    role = models.CharField(
+        max_length=50, default=Roles.DEVELOPER, choices=Roles.choices
+    )
