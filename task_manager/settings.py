f5a800a7f947926e6c171466ae7fef4e806a670e
timofei-nazarov <timofei.nazarov@dualbootpartners.com>
2023-12-25 15:16:10 +0400

    used .get() method instead of direct key access

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -142,10 +142,10 @@ USE_TZ = True
 
 # email settings
 EMAIL_USE_TLS = strtobool(os.environ.get("EMAIL_USE_TLS", "0"))
-EMAIL_HOST = os.environ["EMAIL_HOST"]
-EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
-EMAIL_PORT = os.environ["EMAIL_PORT"]
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 
