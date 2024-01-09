c9c5b9cf56395fcb603afdba4102978ba1ae0135
Timofei Nazarov <132573984+timofei-nazarov@users.noreply.github.com>
2024-01-09 13:23:53 +0400

    Feature/lesson 11 4 test avatar file (#38)
    * refactor with black
    
    * test avatar_picture file

--- task_manager/utils.py
+++ task_manager/utils.py
@@ -1,8 +1,8 @@
 def strtobool(val):
     val = val.lower()
-    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+    if val in ("y", "yes", "t", "true", "on", "1"):
         return 1
-    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+    elif val in ("n", "no", "f", "false", "off", "0"):
         return 0
     else:
         raise ValueError("invalid truth value %r" % (val,))
