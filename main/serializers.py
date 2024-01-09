4d4e31bf1cd6a06707dcdc8d0b064a84935a2232
Timofei Nazarov <132573984+timofei-nazarov@users.noreply.github.com>
2024-01-09 13:08:13 +0400

    added avatar_picture serializer field (#37)

--- main/serializers.py
+++ main/serializers.py
@@ -1,11 +1,33 @@
+from django.conf import settings
+from django.core.files.base import File
+from django.core.validators import FileExtensionValidator
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from .models import User, Task, Tag
 
 
+class FileMaxSizeValidator:
+    def __init__(self, max_size: int) -> None:
+        self.max_size = max_size
+
+    def __call__(self, value: File) -> None:
+        if value.size > self.max_size:
+            raise ValidationError(f"Maximum size {self.max_size} exceeded.")
+
+
 class UserSerializer(serializers.ModelSerializer):
+    avatar_picture = serializers.FileField(
+        required=False,
+        validators=[
+            FileMaxSizeValidator(settings.UPLOAD_MAX_SIZES["avatar_picture"]),
+            FileExtensionValidator(["jpeg", "jpg", "png"]),
+        ]
+    )
+
     class Meta:
         model = User
-        fields = ("id", "username", "first_name", "last_name", "email", "role")
+        fields = ("id", "username", "first_name", "last_name", "email", "role", "avatar_picture")
 
 
 class TagSerializer(serializers.ModelSerializer):
